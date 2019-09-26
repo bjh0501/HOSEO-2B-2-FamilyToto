b71cfc3ad2aefd5460b2278e84eb83cba86744f9
parkjiho <qkrwlgh0501@naver.com>
2019-09-26 15:52:59 +0900

    경험치완성

--- src/main/java/com/familytoto/familytotoProject/dashboard/controller/DashboardController.java
+++ src/main/java/com/familytoto/familytotoProject/dashboard/controller/DashboardController.java
@@ -1,12 +1,15 @@
 package com.familytoto.familytotoProject.dashboard.controller;
 
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.familytoto.familytotoProject.dashboard.service.DashboardService;
 import com.familytoto.familytotoProject.registerCust.domain.CustVO;
@@ -18,12 +21,27 @@ public class DashboardController {
 	DashboardService dashboardService;
 	
 	@RequestMapping("/dashboard")
-	public String dashboard(HttpSession session, Model model) {
-		CustVO cVo = (CustVO) session.getAttribute("cust");
+	public ModelAndView dashboard(HttpSession session,
+			ModelAndView mv,
+			HttpServletResponse response) {
+		CustVO vo = (CustVO) session.getAttribute("cust");
 
-		model.addAttribute("list", dashboardService.getDefaultInfo(cVo.getFamilyCustNo()));
+		if(vo.getFamilyCustNo() == 0) {
+			try {
+				response.setContentType("text/html; charset=UTF-8");
+	            PrintWriter out = response.getWriter();
+	            out.println("<script>alert('연동이 안된 소셜아이디는 대시보드를 볼 수 없습니다. "
+	            		+ "원스포츠 아이디로 연동해주세요.');location.replace('/');</script>");
+	            out.flush();
+	            mv.setViewName("/");
+	            return mv;
+			} catch(Exception e) {}
+		}
 		
-		return "/loginInfo/dashboard";
+		mv.addObject("list", dashboardService.getDefaultInfo(vo.getFamilyCustNo()));
+		mv.setViewName("/loginInfo/dashboard");
+		
+		return mv;
 	}
 	
 	@RequestMapping("/dashboard/getCredit")
